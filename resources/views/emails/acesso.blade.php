fc31e0f290d3ba41fe37b2c0ddfe1095753729b6
Thiago Oliveira <thiagok2@gmail.com>
2018-11-20 10:44:20 -0300

    Usuario - Convite

---
+++ resources/views/emails/acesso.blade.php
@@ -0,0 +1,15 @@
+Olá <strong>{{ $name }}</strong>,
+
+<p>
+Bem vindo a plataforma <a href="#">Normativas</a>.
+<br/>
+Você foi adicionad@ como {{$tipo}} pelo {{$unidade}}.
+<br/>
+Seja bem vindo. Faça seu primeiro acesso com esse email como login, sua senha provisória é senha: <b>{{$password}}</b>
+</p>
+
+<p>
+Atenciosamente<br/>
+--<br/>
+Normativas - NEES
+</p>
