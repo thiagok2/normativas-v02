88d3baf5403c0c5840b0efd6eca7d9a7f846b226
Thiago Oliveira <thiagok2@gmail.com>
2018-11-20 10:44:36 -0300

    Usuario - Convite

--- app/Models/Convite.php
+++ app/Models/Convite.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 use App\User;
+use Mail;
 
 class Convite extends Model
 {
@@ -25,6 +26,23 @@ class Convite extends Model
 
     public function user(){
         return $this->hasOne(User::class);
+    }
+
+    public function enviarNovoUsuario($userNovo,$passwordGerado){
+        $to_name = $userNovo->name;
+        $to_email = 'thiagok2@gmail.com';
+        //$to_email = $userNovo->email;
+        $data = array(
+            'name'      =>  $userNovo->name, 
+            "password"  =>  $passwordGerado,
+            "unidade"   =>  $userNovo->unidade->nome,
+            "tipo"      =>  $userNovo->tipo);
+    
+        Mail::send('emails.acesso', $data, function($message) use ($to_name, $to_email) {
+            $message->to($to_email, $to_name)
+                ->subject('Acesso a plataforma Normativas');
+                $message->from('normativas@nees.com.br','Normativas - NEES');
+        });
     }
 
 
