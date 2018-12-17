9652d5dbd59d8ed72d2cf7f7c0a20814d41c6035
Thiago Oliveira <thiagok2@gmail.com>
2018-12-17 09:21:16 -0300

    email - texto alterado

--- resources/views/emails/acesso.blade.php
+++ resources/views/emails/acesso.blade.php
@@ -3,11 +3,11 @@ Olá <strong>{{ $name }}</strong>,
 <p>
 Bem vindo à plataforma <a href="{{getenv('APP_URL')}}">Normativas - Portal Democrático de Atos normativos de Educação.</a>
 <br/>
-Você foi adicionado(a) pelo Portal Normativas como {{$tipo}} representando o {{$unidade}}.
+Você foi adicionado(a) pelo Portal Normativas como {{$tipo}}(a) representando o {{$unidade}}.
 <br/>
 Faça seu primeiro acesso com os dados abaixo:<br/>
 login(email): {{$email}}<br/>
-senha provisória:: <b>{{$password}}</b>
+senha provisória: <b>{{$password}}</b>
 </p>
 
 Clique aqui para acessar:
