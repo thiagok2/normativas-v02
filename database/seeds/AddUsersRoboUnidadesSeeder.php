f70bd3716c68dc5641578dc842d1eda6f1a3b26c
Thiago Oliveira <thiagok2@gmail.com>
2019-05-20 23:28:27 -0300

    update prod

--- database/seeds/AddUsersRoboUnidadesSeeder.php
+++ database/seeds/AddUsersRoboUnidadesSeeder.php
@@ -55,6 +55,16 @@ class AddUsersRoboUnidadesSeeder extends Seeder
             'tipo' => User::TIPO_EXTRATOR
         ]);
 
+        $unidadeDF = Unidade::where('sigla', "CEE-DF")->first();
+
+        $usuarioRobo5 = User::create([
+            'name' => "Robô extrator CEE-DF",
+            'email' => "ceedf@extrator.com.br",
+            'password' => Hash::make('extrator'),
+            'unidade_id' => $unidadeDF->id,
+            'tipo' => User::TIPO_EXTRATOR
+        ]);
+
 
     }
 }
